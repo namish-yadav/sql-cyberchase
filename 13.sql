f677cd937efb935613025fa8d9e02bcf7d581240
Namish-yadav <namishyadavv@gmail.com>
2026-06-16 06:15:38 +0530

    Add Cyberchase SQL solutions

---
+++ 13.sql
@@ -0,0 +1,15 @@
++----------------------------------------+--------+
+|                 title                  | season |
++----------------------------------------+--------+
+| Unhappily Ever After                   | 6      |
+| Escape From Merlin's Maze              | 6      |
+| Step By Step                           | 6      |
+| Team Spirit                            | 6      |
+| Jimaya Jam                             | 6      |
+| A Perfect Score                        | 6      |
+| Chaos as Usual                         | 6      |
+| Weather Watchers Gone With The Fog     | 7      |
+| The Emperor Has Snow Clothes           | 7      |
+| The X-Factor                           | 7      |
+| Blowin' in the Wind Blowin in the Wind | 7      |
++----------------------------------------+--------+
